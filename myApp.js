88c204de101522524e3e8d10ba40066c6b8151af
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-05-16 12:17:22 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,15 @@
 let express = require('express');
 let app = express();
 
+let filepath = __dirname + "/views/index.html";
+
+app.use("/public",express.static(__dirname + "/public"));
+
+app.get("/", (req,res)=>{
+    // res.send("Hello Express")
+    res.sendFile(filepath)
+})
+
 
 
 
